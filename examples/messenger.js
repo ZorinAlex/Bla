581b5cf5e980b13df7c0a81ac0c2cd4ff60add8d
ZorinAlex <Zorinuk@gmail.com>
2016-09-28 13:48:41 +0300

    test

--- examples/messenger.js
+++ examples/messenger.js
@@ -156,7 +156,19 @@ const actions = {
       var location = firstEntityValue(entities, "location");
       var date = firstEntityValue(entities, "datetime");
       if (location || date) {
-        let message = 'in '+ location?location:'your location '+'at '+ date?date:'now';
+        var message = 'in ';
+        if(location)
+        {
+          message+=location;
+        }else{
+          message+='your location ';
+        }
+        if(date)
+        {
+          message+=date;
+        }else{
+          message+='today ';
+        }
         //context.forecast = 'in '+ location?location:'your location '+'at '+ date?date:'now'; // we should call a weather API here
         context.forecast = message;
         //delete context.missingLocation;
